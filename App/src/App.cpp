2b72d2c5a3972a38e65134fa45fec18872eb29d5
Cargnx <117575681+Altaiir1@users.noreply.github.com>
2025-12-19 11:01:59 +0100

    [WIP] Setup vertices and VBO buffer

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -50,6 +50,18 @@ App::App() : window(nullptr), initialized(false), imgui(nullptr)
 
     imgui = std::make_unique<ImGuiHandler>(window);
 
+    // Vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     // Returns true in the isValid() function so everything went fine
     initialized = true;
 }
